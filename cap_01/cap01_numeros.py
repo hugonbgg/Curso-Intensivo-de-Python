a65488102f868ea09bddf1183a01d257e2020680
hugonbgg <hugonbgg@gmail.com>
2020-02-19 20:03:36 -0300

    Capitulo 01 - Terminado

---
+++ cap_01/cap01_numeros.py
@@ -0,0 +1,36 @@
+#Inteiros
+
+2 + 3
+3 - 2
+2 * 3
+3 / 2
+
+3 ** 2
+3 ** 3
+10 ** 6
+
+2 + 3 * 4
+(2 + 3) * 4
+
+#Pontos flutuantes (float)
+
+0.1 + 0.1
+0.2 + 0.2
+2 * 0.1
+2 * 0.2
+
+0.2 + 0.1
+3 * 0.1
+
+#Função str()
+
+age = 23
+message = "Happy " + age + "rd Birthday!"
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+####################
+print(7 + 1)
+print(16 - 8)
+print(2 * 4)
+print(64 / 8)
