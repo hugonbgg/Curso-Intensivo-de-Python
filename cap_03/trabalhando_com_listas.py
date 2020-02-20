c37a5afb0a3cc8bad3d1674c8cca400db2d45494
hugonbgg <hugonbgg@gmail.com>
2020-02-20 16:28:20 -0300

    Capitulo 03

---
+++ cap_03/trabalhando_com_listas.py
@@ -0,0 +1,26 @@
+#Laços for
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+
+
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+print ('Thank you, everyone. That was a great magic show!')
+
+############################
+
+for magician in magicians:
+    print(magician)
+
+
