0434245cf4a4f8b0fb96dada74c8bf835e632d97
hugonbgg <hugonbgg@gmail.com>
2020-02-19 21:16:56 -0300

    Capitulo 02

---
+++ cap_02/introducao_as_listas.py
@@ -0,0 +1,120 @@
+# Colchetes indicam que é uma lista e os elementos são separados por vírgulas.
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print (bicycles)
+type (bicycles)
+
+#Acessando elementos da lista
+#A posição dos índices começa em 0 e não em 1
+print(bicycles[0])
+
+
+#Métodos de string podem ser usados com elementos da lista
+print(bicycles[0].title())
+
+#Acessando o último elemento da lista
+print(bicycles[-1])
+
+#Usando valores individuais de uma lista
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+
+nomes = ['João', 'Maria', 'Ana', 'Pedro']
+print ("Olá " + nomes[0] + " !!!")
+
+#Alterando, acrescentando e removendo elementos da lista
+
+#Alterando/substituindo
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print (motorcycles)
+
+motorcycles [0] = 'ducati'
+print(motorcycles)
+
+#Acrescentando
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+#método append()
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+#Criando uma lista vazia e adicionando os elementos
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+print(motorcycles)
+
+#método insert
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+#removendo
+
+#método del() - Quando se sabe a posição do item que quer remover.
+#Não permite mais acessar o valor que foi removido da lista.
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+
+#método pop() Remove o último item de uma lista. Permite acessar o valor que foi removido da lista.
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print (motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print (popped_motorcycle)
+print (motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+
+print("The last motorcyce I owned was a " + last_owned.title() + ".")
+
+
+#Removendo itens de qualquer posição usando o método pop()
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print ('The first motorcycle I owned was a ' + first_owned.title() + '.')
+
+#Removendo um item de acordo com o valor com o método remove()
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print (motorcycles)
+
+motorcycles.remove('ducati')
+print (motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+too_expensive = 'ducati'
+
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\n A " + too_expensive.title() + " is a too expensive for me.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
