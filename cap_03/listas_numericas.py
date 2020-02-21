a1ebcdcac0c3b75cd3dbab64e04c78b0fd764095
hugonbgg <hugonbgg@gmail.com>
2020-02-21 10:22:45 -0300

    capitulo_03

--- cap_03/listas_numericas.py
+++ cap_03/listas_numericas.py
@@ -27,3 +27,38 @@ for value in range (1,11):
     squares.append(value ** 2)
 
 print(squares)
+
+#Estatísticas simples com lista de números
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+min(digits)
+max(digits)
+sum(digits)
+
+#List comprehension (abrangência da lista)
+
+squares = [value ** 2 for value in range (1,11)]
+print (squares)
+
+#Exercicios
+
+numeros = [print (numero) for numero in range (1,21)]
+
+milhao = [print (numero) for numero in range (1, 1000001)]
+milhao = list(range(1,1000001))
+min(milhao)
+max(milhao)
+sum(milhao)
+
+impares = [print (impar) for impar in range (1,21,2)]
+
+multiplos_de_tres = [print(multiplo) for multiplo in range (3, 30,3)]
+
+ao_cubo = []
+for value in range(1,11):
+    ao_cubo.append(value**3)
+
+print(ao_cubo)
+
+ao_cubo = print([(value**3) for value in range(1,11)])
